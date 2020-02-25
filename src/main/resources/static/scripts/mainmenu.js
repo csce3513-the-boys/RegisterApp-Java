93146bc8c1e3cb5b3ba461e1af49a983148a7d40
Jackson <jplacrue@uark.edu>
2020-02-25 17:56:19 -0600

    Added buttons

--- src/main/resources/static/scripts/mainmenu.js
+++ src/main/resources/static/scripts/mainmenu.js
@@ -1,6 +1,30 @@
-document.getElementById("button").addEventListener("click", myFunction);
+document.getElementById("button1").addEventListener("click", startTransaction);
+document.getElementById("button2").addEventListener("click", viewProducts);
+document.getElementById("button3").addEventListener("click", createEmployee);
+document.getElementById("button4").addEventListener("click", salesReport);
+document.getElementById("button5").addEventListener("click", cashierReport);
 
-function myFunction()
+function startTransaction()
+{
+    alert("Functionality has not been implemented");
+}
+
+function viewProducts()
+{
+    alert("Functionality has not been implemented");
+}
+
+function createEmployee()
+{
+    alert("Functionality has not been implemented");
+}
+
+function salesReport()
+{
+    alert("Functionality has not been implemented");
+}
+
+function cashierReport()
 {
     alert("Functionality has not been implemented");
 }
